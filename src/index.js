618b301b200a905831ca766455edde2f047020f2
Glauco Pater <>
2019-05-30 19:51:49 +0200

    Fixed connections

--- src/index.js
+++ src/index.js
@@ -8,12 +8,11 @@ import "./styles.css";
 const MAX_ITEMS = 5;
 
 function App() {
-
   const randomInt = () => {
     return Math.random() < 0.5;
   };
 
-  const generateRandomProp = (index) => {
+  const generateRandomProp = index => {
     return {
       pos: { x: index, y: 0 },
       top: randomInt(),
@@ -22,65 +21,41 @@ function App() {
       left: randomInt(),
       rightConnected: null,
       leftConnected: null
-    }
-  }
+    };
+  };
 
-  const connectItems = (itemsWithProps) => {
-    const rightConnectedItems = 
-    itemsWithProps.map((item,index)=>{
+  const connectItems = itemsWithProps => {
+    const rightConnectedItems = itemsWithProps.map((item, index) => {
       //every check and update should be atomic
-      //connections could be 4
-      //first check right with next left
-      if(itemsWithProps[index+1]){
-        if(item.props.right && (item.props.right === itemsWithProps[index+1].props.left)){
-          const { rightConnected, ...props } = item.props;
-          const newItem =  React.cloneElement(item, {
-            rightConnected: true,
-            ...props
-          });
-
-              //second check left with previous right, I just need to know if it was rightConnected
-          if(itemsWithProps[index-1]){
-            console.log("checking connection ",index, index-1, newItem.props.left, itemsWithProps[index-1].props.right );
-            if(newItem.props.left && (newItem.props.left === itemsWithProps[index-1].props.right)){
-            const { leftConnected, ...props } = newItem.props;
-              return React.cloneElement(item, {
-                leftConnected: true,
-                ...props
-              });
-            }
-else
-          return newItem;
-        }
-        else return newItem;
-      }
-
-        else return item;
-      }
-
-      else return item;
-    })
+      //connections could be 4...
+        const { rightConnected, leftConnected, ...props } = item.props;
+        return React.cloneElement(item, {
+          rightConnected: itemsWithProps[index + 1] && itemsWithProps[index + 1].props.left && item.props.right,
+          leftConnected: itemsWithProps[index - 1] && itemsWithProps[index - 1].props.right && item.props.left,
+          ...props
+        });
+    });
 
     return rightConnectedItems;
-
-  }
+  };
 
   const generateItems = () => {
     const items = [...Array(MAX_ITEMS).keys()];
-    const itemsWithProps =
-      items.map((item, index) => {
+    const itemsWithProps = items.map((item, index) => {
       const randomProps = generateRandomProp(index);
-      return <Item key={index} {...randomProps}>{index}</Item>;
-    })
+      return (
+        <Item key={index} {...randomProps}>
+          {index}
+        </Item>
+      );
+    });
 
     return connectItems(itemsWithProps);
-  }
+  };
 
   return (
     <div className="App">
-      <StyledGrid>
-        {generateItems()}
-      </StyledGrid>
+      <StyledGrid>{generateItems()}</StyledGrid>
     </div>
   );
 }
