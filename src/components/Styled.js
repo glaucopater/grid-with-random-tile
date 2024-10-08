2a612bc988d79f7e9fe27285f408da5c2b35a30f
Glauco Pater <glauco.pater@gmail.com>
2024-10-08 17:52:42 +0200

    remove styled-icons update jss

--- src/components/Styled.js
+++ src/components/Styled.js
@@ -8,7 +8,7 @@ export const StyledGrid = styled.div`
   grid-template-rows: 100px 100px;
   grid-gap: 10px;
   margin: 50px auto;
-  width: 30%;
+  width: 21%;
   max-width: 600px
   @media (max-width: 800px) {
     margin: 50px;
